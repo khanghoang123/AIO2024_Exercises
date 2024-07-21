fcd7ea940b60d416bc3f0e78ecc9d330a30a3e81
khanghoang123 <nguyenhoangkhang040904@gmail.com>
2024-07-21 16:19:32 +0700

    add module_1

--- Week4/object_detection.py
+++ Week4/object_detection.py
@@ -3,8 +3,8 @@ import numpy as np
 from PIL import Image
 import streamlit as st
 
-MODEL = "../model/MobileNetSSD_deploy.caffemodel"
-PROTOTXT = "../model/MobileNetSSD_deploy.prototxt.txt"
+MODEL = "../Model/MobileNetSSD_deploy.caffemodel"
+PROTOTXT = "../Model/MobileNetSSD_deploy.prototxt.txt"
 
 
 def process_image(image):
